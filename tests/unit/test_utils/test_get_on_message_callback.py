b18fd8035d94de5358f4a038ca8cc5e42b225f96
jakub-figat <jakub.figat@skygate.io>
2022-03-04 20:48:36 +0100

    Add more tests for get_on_message_callback function

--- tests/unit/test_utils/test_get_on_message_callback.py
+++ tests/unit/test_utils/test_get_on_message_callback.py
@@ -3,7 +3,8 @@ from typing import Callable
 
 import pytest
 from _pytest.logging import LogCaptureFixture
-from pydantic import BaseModel
+from kombu import Message
+from pydantic import BaseModel, validator
 
 from cornflower.utils import get_on_message_callback
 from tests.unit.test_utils.conftest import FakeMessage
@@ -18,6 +19,18 @@ def fake_message() -> FakeMessage:
 
 
 @pytest.fixture
+def fake_message_with_invalid_json() -> FakeMessage:
+    fake_message = FakeMessage(content={"arg": "text"})
+    fake_message._content = "'{'arg': invalid}'"
+    return fake_message
+
+
+@pytest.fixture
+def fake_message_with_invalid_data() -> FakeMessage:
+    return FakeMessage(content={"arg": "invalid"})
+
+
+@pytest.fixture
 def acked_fake_message() -> FakeMessage:
     fake_message = FakeMessage(content={"arg": "acked"})
     fake_message.ack()
@@ -26,6 +39,13 @@ def acked_fake_message() -> FakeMessage:
 
 class PydanticModel(BaseModel):
     arg: str
+
+    @validator("arg")
+    def validate_arg(cls, value) -> str:
+        if value == "invalid":
+            raise ValueError("Invalid arg")
+
+        return value
 
 
 @pytest.fixture
@@ -36,16 +56,50 @@ def callable_with_pydantic_model() -> Callable[[PydanticModel], None]:
     return _callable
 
 
-def test_get_on_message_callback_with_callable_with_side_effect(
-    callable_with_pydantic_model: Callable[[PydanticModel], None], fake_message: FakeMessage, caplog: LogCaptureFixture
-) -> None:
+@pytest.fixture
+def on_message_callback_with_pydantic_model(
+    callable_with_pydantic_model: Callable[[PydanticModel], None]
+) -> Callable[[Message], None]:
     routing_key = "test"
 
-    on_message_callback = get_on_message_callback(
+    return get_on_message_callback(
         _callable=callable_with_pydantic_model, routing_key=routing_key, pydantic_model_class=PydanticModel
     )
 
-    with caplog.at_level(logging.INFO):
-        on_message_callback(fake_message)
 
+def test_get_on_message_callback_with_callable_with_side_effect(
+    on_message_callback_with_pydantic_model: Callable[[Message], None],
+    fake_message: FakeMessage,
+    caplog: LogCaptureFixture,
+) -> None:
+    with caplog.at_level(logging.INFO):
+        on_message_callback_with_pydantic_model(fake_message)
+
+    assert fake_message.is_ack
     assert fake_message.content["arg"] in caplog.text
+
+
+def test_get_on_message_callback_logs_error_on_invalid_json(
+    on_message_callback_with_pydantic_model: Callable[[Message], None],
+    fake_message_with_invalid_json: FakeMessage,
+    caplog: LogCaptureFixture,
+) -> None:
+    expected_log = f"Error while parsing JSON for {PydanticModel.__name__}"
+    with caplog.at_level(logging.ERROR):
+        on_message_callback_with_pydantic_model(fake_message_with_invalid_json)
+
+    assert fake_message_with_invalid_json.is_ack
+    assert expected_log in caplog.text
+
+
+def test_get_on_message_callback_logs_error_on_pydantic_validation_error(
+    on_message_callback_with_pydantic_model: Callable[[Message], None],
+    fake_message_with_invalid_data: FakeMessage,
+    caplog: LogCaptureFixture,
+) -> None:
+    expected_log = f"Error while parsing {PydanticModel.__name__}"
+    with caplog.at_level(logging.ERROR):
+        on_message_callback_with_pydantic_model(fake_message_with_invalid_data)
+
+    assert fake_message_with_invalid_data.is_ack
+    assert expected_log in caplog.text
